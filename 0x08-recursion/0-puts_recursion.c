a99054caa4214ef4c9dab08efcdf0a0b5a8c20e8
Ashagrie <yalelet16@gmail.com>
2022-07-20 11:16:36 -0700

    error is fixed

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse
- * @s: string
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to print
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: void
  */
-void _print_rev_recursion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s != '\0')
+	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		_putchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
 
